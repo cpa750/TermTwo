6f979523dc95f505cd13714de4a07f9c20fe8005
Cian <cianjpayne@gmail.com>
2019-02-12 22:44:52 +0000

    Pushing the Lab 4 and Lab 2 cmake build folder

---
+++ C++/Lab 4/cmake-build-debug/CMakeFiles/Lab_4.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Lab_4.dir/h2.cpp.o"
+  "Lab_4.pdb"
+  "Lab_4"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Lab_4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
